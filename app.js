8e12fef3cd5552bc83336b0d7804b33b35afe11a
venkatasai24 <103911586+venkatasai24@users.noreply.github.com>
2023-03-04 21:19:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const request = require("request");
+
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/",function(req,res)
+{
+    const first=req.body.firstName;
+    const last=req.body.lastName;
+    const email=req.body.email;
+    const data = {
+        members : [
+            {
+                email_address: email ,
+                status : "subscribed" ,
+                merge_fields :{
+                    FNAME : first ,
+                    LNAME : last 
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/" + process.env.listId ;
+
+    const options = {
+        method:"POST",
+        auth : "venkatasai:" + process.env.apiKey 
+    }
+
+    const request=https.request(url,options,function(response)
+    {
+        if(response.statusCode === 200 )
+        {
+            res.sendFile(__dirname+"/success.html");
+        }
+        else
+        {
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function(data)
+        {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/failure", function (req,res){
+  res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("3000 port is set up!!");
+})
+
